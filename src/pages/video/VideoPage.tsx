b218dbfc07c5b8bbe843019c78add28fb49b32c1
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-11-13 14:49:24 +0100

    added video player and video

--- src/pages/video/VideoPage.tsx
+++ src/pages/video/VideoPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import weLoveAargau from '../../res/videos/weLoveAargau.mp4';
 import './VideoPage.scss';
 
 export const VideoPage: React.FC = () => {
 
   return (
     <div className='VideoPage full-screen'>
-      sup
+      <ReactPlayer url={weLoveAargau}
+                   playing
+                   loop
+                   className='player'/>
     </div>
   )
 
