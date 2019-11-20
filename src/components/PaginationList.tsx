45bd5d5760325cc405cb038deddb4a44f92580e7
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2019-11-20 17:20:54 +0100

    created pagination slideshow

---
+++ src/components/PaginationList.tsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import './PaginationList.scss'
+
+interface PaginationListProps {
+  setElementIndex: Function,
+  nofElements: number,
+  activeIndex: number
+
+}
+
+export class PaginationList extends Component<PaginationListProps> {
+
+
+  getNofItems() {
+    let items = [];
+    for (let i = 0; i < this.props.nofElements; i++) {
+      items.push(<div className={`pagination-button ${this.props.activeIndex === i ? 'enabled' : 'disabled'}`}
+                      onClick={() => this.props.setElementIndex(i)}
+                      key={i}/>)
+    }
+    return items
+  }
+
+  render() {
+
+    return (
+      <div className={'pagination-list'}>
+        {this.getNofItems()}
+      </div>
+
+    )
+
+  }
+
+}
--- src/pages/intro/IntroductionPage.tsx
+++ src/pages/intro/IntroductionPage.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {PaginationList} from '../../components/PaginationList';
 import {RoundButton} from '../../components/RoundButton';
 import iconArrowLeft from '../../res/icons/arrow_left.svg'
 import iconArrowRight from '../../res/icons/arrow_right.svg'
@@ -6,7 +7,6 @@ import './IntroductionPage.scss';
 import {Slide01} from './Slide01';
 import {Slide02} from './Slide02';
 import {Slide03} from './Slide03';
-
 
 export const IntroductionPage: React.FC = () => {
   const items = [<Slide01/>, <Slide02/>, <Slide03/>]
@@ -26,6 +26,11 @@ export const IntroductionPage: React.FC = () => {
     setActivePage(items[nextIndex])
   }
 
+  const setElementIndex = (index:number) => {
+    console.log(index)
+    setActiveIndex(index)
+  }
+
   return (
     <div className='IntroductionPage'>
 
@@ -36,10 +41,16 @@ export const IntroductionPage: React.FC = () => {
                      isActive={activeIndex !== 0}
                      icon={iconArrowLeft}
                      size='3.5rem'/>
+        <PaginationList
+          setElementIndex={setElementIndex}
+          activeIndex={activeIndex}
+          nofElements={items.length}/>
         <RoundButton onClick={next}
                      isActive={activeIndex < items.length - 1}
                      icon={iconArrowRight}
                      size='3.5rem'/>
+
+
       </div>
     </div>
   )
