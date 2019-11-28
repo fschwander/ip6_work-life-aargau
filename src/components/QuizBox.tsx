901a553102c30b9c89ee0a465b741fee15692efc
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-11-28 15:10:08 +0100

    flipping flow of Question to top when line would cross the container

--- src/components/QuizBox.tsx
+++ src/components/QuizBox.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {CSSProperties, useState} from 'react';
 import {EntryLabel} from './EntryLabel';
 import {HoverPoint} from './HoverPoint';
 import {Quiz} from './Quiz';
@@ -30,8 +30,18 @@ export const QuizBox: React.FC<QuizBoxProps> = (props) => {
   const [isOpen, setIsOpen] = useState(false)
   const orientation = props.orientation === undefined ? 'left' : props.orientation;
 
+  const getVerticalOrientation = () => {
+    if (orientation === 'left' && props.lineRotationInDeg > 270) {
+      return 'column-reverse'
+    } else {
+      return 'column'
+    }
+  }
+
+  const verticalOrientation: CSSProperties = {flexDirection: getVerticalOrientation()}
+
   return (
-    <div className={`QuizBox question-container ${props.orientation}`}>
+    <div className={`QuizBox question-container ${props.orientation}`} style={verticalOrientation}>
       <div className='label'>
         <EntryLabel text={props.question.title}
                     lineWidth={props.lineWidth}
