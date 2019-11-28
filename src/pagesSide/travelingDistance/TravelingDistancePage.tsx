75bc111cae6abb7f52b843e0d03d1e4c9890c256
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-11-28 16:35:38 +0100

    new component for ivis

---
+++ src/pagesSide/travelingDistance/TravelingDistancePage.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './TravelingDistancePage.scss'
+
+export class TravelingDistancePage extends Component {
+
+  render() {
+    return(
+      <div className='TravelingDistancePage'>
+        foo
+
+      </div>
+    )
+  }
+}
