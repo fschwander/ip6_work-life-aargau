a5e640b34d35ec85ae2ba8431589fb7211c76f13
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-12-06 17:13:05 +0100

    added Alert when app is opened on mobile screen

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {UncontrolledAlert} from 'reactstrap';
 import './App.scss';
 
 const App: React.FC = (props) => {
 
+  const throwAlertOnMobile = () => {
+    console.log(window.innerWidth);
+    if(window.innerWidth < 600 || window.innerHeight < 800) {
+      return (
+        <UncontrolledAlert>
+          <h2>Mobile nicht unterstützt</h2>
+          <p>Bei dieser Website handelt es sich um einen Prototypen und kleine Screens werden noch nicht
+            unterstützt.</p>
+          <p>Wir empfehlen auf einen grossen Bildschirm zu wechseln. </p>
+        </UncontrolledAlert>)
+    }
+  }
+
   return (
     <div className="App">
       {props.children}
+      {throwAlertOnMobile()}
     </div>
   );
 };
