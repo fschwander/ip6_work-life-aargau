e025db8d1362590ccd87f39b68aa85be1838c1a5
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-12-08 21:18:42 +0100

    added method to remove node from DOM, also with no passed method

--- src/components/widgets/PopupContainer.tsx
+++ src/components/widgets/PopupContainer.tsx
@@ -3,16 +3,26 @@ import closeIcon from '../../res/icons/close.svg';
 import {RoundButton} from '../buttons/RoundButton';
 
 interface Props {
-  onClose: Function
+  onClose?: Function
 }
 
-export const PopupContainer: React.FC<Props> = props => {f
+export const PopupContainer: React.FC<Props> = props => {
+  let rootRef = React.createRef<HTMLDivElement>();
+
+  const closeNode = () => {
+    if (props.onClose !== undefined) {
+      props.onClose();
+    } else if (rootRef.current != null) {
+      // TODO: is not optimal, add better way to remove this component or children
+      rootRef.current.remove()
+    }
+  }
 
   return (
     <div className='PopupContainer full-screen'>
       <div className='content-container'>
         {props.children}
-        <RoundButton onClick={props.onClose} icon={closeIcon} isActive={true}/>
+        <RoundButton onClick={closeNode} icon={closeIcon} isActive={true}/>
       </div>
     </div>
   )
