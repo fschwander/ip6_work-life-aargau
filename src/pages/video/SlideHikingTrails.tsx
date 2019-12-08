584f127f89e6df3e44370229b8ffd6e179fe7e1e
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-12-08 16:39:05 +0100

    added placeholder content as image (svg)

--- src/pages/video/SlideHikingTrails.tsx
+++ src/pages/video/SlideHikingTrails.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {RoundButton} from '../../components/buttons/RoundButton';
 import closeIcon from '../../res/icons/close.svg';
+import {ReactComponent as PlaceholderImg} from './placeholderContent.svg';
+
 
 interface Props {
   onClose: Function
@@ -11,8 +13,8 @@ export const SlideHikingTrails: React.FC<Props> = props => {
   return (
     <div className='SlideHikingTrails slide full-screen'>
       <div className='content-container'>
+        <PlaceholderImg/> {/*todo: remove*/}
         <RoundButton onClick={props.onClose} icon={closeIcon} isActive={true}/>
-        Hiking
       </div>
 
     </div>
