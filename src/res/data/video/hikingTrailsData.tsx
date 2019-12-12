685a4f68c101d131b48539017f217d01d67f11d9
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-12-12 14:29:20 +0100

    forgot to include file of hiking trails data

---
+++ src/res/data/video/hikingTrailsData.tsx
@@ -0,0 +1,52 @@
+import {VideoOverlayInterface} from './VideoOverlayInterface'
+import iconClimbing from '../../icons/climbing.svg'
+import iconHiking from '../../icons/hiking.svg'
+import imgHikingTrails from '../../imgs/ol_hikingtrails.jpg'
+
+export const hikingTrailsData: VideoOverlayInterface = {
+  header: {
+    title: "Wanderwege",
+    text: "Wanderer sind auf den gut beschilderten Wegen unterwegs und staunen über die landschaftliche Vielfalt der Region.",
+    items: [
+      {
+        text: "Anzahl Wanderwege",
+        value: "218"
+      },
+      {
+        text: "Länge",
+        value: "6530 km"
+      },
+      {
+        text: "Höchster Punkt",
+        value: "1200 m ü. M."
+      }
+    ]
+  },
+  topics: [
+    {
+      icon: iconClimbing,
+      text: "Sport"
+    },
+    {
+      icon: iconHiking,
+      text: "Freizeit"
+    }
+  ],
+  graph: {
+    title: "Schwierigkeitsgrade der Wege"
+  },
+  details: {
+    image: imgHikingTrails,
+    title: "Wein-Wanderung",
+    items: [
+      {
+        text: "Länge",
+        value: "32 km"
+      },
+      {
+        text: "Schwierigkeit",
+        value: "leicht"
+      }
+    ]
+  }
+}
