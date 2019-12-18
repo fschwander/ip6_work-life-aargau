8d6888b6113305325ed3dc00a6459e0c51f76cc6
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2019-12-18 09:35:25 +0100

    renamed video page to single video slide and moved component treating component now as child component of a parent page

--- src/pages/video/VideoPage.tsx
+++ src/pages/video/VideoPage.tsx
@@ -1,61 +1,14 @@
 import React from 'react';
-import {OverlayHoverPoint} from '../../components/buttons/OverlayHoverPoint';
-import {BackgroundVideo} from '../../components/containers/BackgroundVideo';
-import {PopupContainer} from '../../components/containers/PopupContainer';
-import {LocationLabel} from '../../components/labels/LocationLabel';
-import {hikingTrails} from '../../res/data/video/hikingTrails'
-import {mammut} from '../../res/data/video/mammut';
-import videoBaden from '../../res/videos/baden.mp4';
+import {VideoSlide} from './VideoSlide';
 
 export const VideoPage: React.FC = () => {
-  const [isPlaying, setIsPlaying] = React.useState(true)
-  const [popupComponent, setPopupContainer] = React.useState();
-
-  const pauseVideo = () => {
-    setIsPlaying(false)
-  }
-
-  const playVideo = () => {
-    if (popupComponent === undefined) {
-      setIsPlaying(true)
-    }
-  }
-
-  const openOverlay = (overlay: any) => {
-    setPopupContainer(overlay)
-    setIsPlaying(false);
-  }
-
-  const closeOverlay = () => {
-    setPopupContainer(undefined);
-    setIsPlaying(true)
-  }
+  const vSlides = [
+    <VideoSlide/>
+  ]
 
   return (
-    <div className='VideoPage full-screen'>
-      <BackgroundVideo source={videoBaden} playVideo={isPlaying}/>
-
-      <div className={`overlay-hover-point-container ${popupComponent === undefined ? 'show' : 'hide'}`}>
-        <OverlayHoverPoint className={`hiking-trails`}
-                           data={hikingTrails}
-                           onMouseEnter={pauseVideo}
-                           onMouseLeave={playVideo}
-                           onPointClicked={openOverlay}/>
-        <OverlayHoverPoint className={`cie-mammut`}
-                           data={mammut}
-                           hOrientation='left'
-                           onMouseEnter={pauseVideo}
-                           onMouseLeave={playVideo}
-                           onPointClicked={openOverlay}/>
-      </div>
-
-      <LocationLabel/>
-
-      {popupComponent !== undefined ?
-        <PopupContainer onCloseButtonClicked={closeOverlay}>
-          {popupComponent}
-        </PopupContainer> : null}
-
+    <div className='VideoPage'>
+      {vSlides[0]}
     </div>
   )
 }
