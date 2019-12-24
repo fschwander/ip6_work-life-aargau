5f7e97acac6085aac434cbe6cd4f37976c305f92
FabianSchwander <fabian.schwander@students.fhnw.ch>
2019-12-24 01:39:50 +0100

    added awesome happy new year content

--- src/pages/video/data/videoSlides.tsx
+++ src/pages/video/data/videoSlides.tsx
@@ -3,6 +3,7 @@ import videoAarau1 from "../../../res/videos/aarau1.mp4";
 import videoAarau2 from "../../../res/videos/aarau2.mp4";
 import {hikingTrails} from "./hikingTrails";
 import {mammut} from "./mammut";
+import {ReactComponent as svgAarau1} from "../../../res/videos/aarau1.svg";
 import {ReactComponent as svgRuine} from "../../../res/videos/baden1-ruine.svg";
 import {ReactComponent as svgRoof} from "../../../res/videos/baden1-roof.svg";
 import {ReactComponent as svgTower} from "../../../res/videos/baden1-tower.svg";
@@ -17,9 +18,9 @@ export const videoSlides: Array<VideoSlideProps> = [
     hoverPoints: [{
       title: 'Kultur & Geschichte',
       subtitle: 'Historische Highlights',
-      className: 'culture-and-history',
+      className: 'penis',
       overlayData: hikingTrails,
-      svgComponent: svgRoof,
+      svgComponent: svgAarau1,
       lineLength: 60,
       lineRotation: 70,
       orientation: 'right'
