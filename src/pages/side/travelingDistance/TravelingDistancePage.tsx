4b9f66f901adac573b9f3992b69d08b597e2bd4c
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-08 15:29:34 +0100

    added svg for map

--- src/pages/side/travelingDistance/TravelingDistancePage.tsx
+++ src/pages/side/travelingDistance/TravelingDistancePage.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import {SelectionChip} from '../../../components/buttons/SelectionChip';
+import {ReactComponent as MapSVG} from '../../../res/imgs/map.svg'
 
 export const TravelingDistancePage: React.FC = () => {
+
+
 
   return (
     <div className='TravelingDistancePage'>
       <div className='TravellingIntroduction'>
-      <h2>Der Aargau is gut vernetzt</h2>
-      <p><b>DER KANTON, der im Zentrum der Schweiz und Europas liegt.</b></p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt ipsum ac erat mattis, in imperdiet lectus sagittis. </p>
+        <h2>Der Aargau is gut vernetzt</h2>
+        <p><b>DER KANTON, der im Zentrum der Schweiz und Europas liegt.</b></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt ipsum ac erat mattis, in imperdiet
+          lectus sagittis. </p>
       </div>
       <h3>Karte Einstellungnen</h3>
 
-        <SelectionChip
-              text={'Zugnetz'}/>
+      <SelectionChip
+        text={'Zugnetz'}/>
+
+      <MapSVG/>
 
     </div>
   )
