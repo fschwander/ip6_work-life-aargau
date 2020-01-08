347c59ba54ad7eb9af3a3f5c74680cc9a809b00f
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-08 20:41:30 +0100

    added json data for city points

--- src/pages/side/travelingDistance/TravelingDistancePage.tsx
+++ src/pages/side/travelingDistance/TravelingDistancePage.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef, useState} from 'react';
 import {SelectionChip} from '../../../components/buttons/SelectionChip';
 import {ReactComponent as MapSVG} from '../../../res/imgs/map.svg';
 import * as d3 from 'd3';
+import citiesData from './citiesData.json'
 
 export const TravelingDistancePage: React.FC = () => {
   const isInitialMount = useRef(true)
@@ -11,42 +12,65 @@ export const TravelingDistancePage: React.FC = () => {
   const [cantonsVisible, setCantonsVisible] = useState(true);
   const [citiesVisible, setCitiesVisible] = useState(true);
   const [trainsVisible, setTrainsVisible] = useState(true);
+  // const [airportsVisible, setAirportsVisible] = useState(true);
   // const [motorwaysVisible, setMotorwaysVisible] = useState(true);
 
   const initMap = () => {
     console.log('init');
-
-
+    setupCities()
   }
 
   const updateChart = () => {
     console.log('update');
-    const mapSVG = d3.select(svgRef.current)
     // console.log(svgRef, svgRef.current, mapSVG);
+    const mapSVG = d3.select(svgRef.current)
 
     mapSVG.selectAll('#lakes')
-      .attr('opacity',lakesVisible ? 1 : 0)
+      .attr('opacity', lakesVisible ? 1 : 0)
 
     mapSVG.selectAll('#trains')
-      .attr('opacity',trainsVisible ? 1 : 0)
+      .attr('opacity', trainsVisible ? 1 : 0)
 
-
-
+    mapSVG.selectAll('#cantons')
+      .attr('opacity', cantonsVisible ? .5 : 0)
   }
 
+  const setupCities = () => {
+    const cityGroup = d3.select(svgRef.current).select('#cities')
+      .selectAll('g')
+      .data(citiesData).enter().append('g')
+
+    cityGroup.attr('transform', d => {
+      console.log(d);
+      return `translate(${d.x},${d.y})`
+    })
+
+    cityGroup.append('circle')
+      .attr('r', 5)
+      .attr('cx', 13)
+      .attr('cy', 13)
+      .attr('fill', 'white')
+
+    cityGroup.append('circle')
+      .attr('r', 13)
+      .attr('cx', 13)
+      .attr('cy', 13)
+      .attr('opacity', 0.2)
+      .attr('fill', 'white')
+      .attr('class', 'button')
+      .on('click', d => console.log('click', d)) // todo: set state of active city here
+
+    console.log(citiesData);
+  }
 
   useEffect(() => {
-    if(isInitialMount.current) {
+    if (isInitialMount.current) {
       isInitialMount.current = false;
       initMap();
     }
     updateChart()
 
   })
-
-
-
-
 
   return (
     <div className='TravelingDistancePage'>
@@ -58,11 +82,15 @@ export const TravelingDistancePage: React.FC = () => {
       </div>
       <h3>Karte Einstellungnen</h3>
 
+      <div className='chipWrapper'>
       <SelectionChip text={'Zug'}
                      onClick={() => setTrainsVisible(!trainsVisible)}/>
       <SelectionChip text={'Seen'}
                      onClick={() => setLakesVisible(!lakesVisible)}/>
+      <SelectionChip text={'Kantonen'}
+                     onClick={() => setCantonsVisible(!cantonsVisible)}/>
 
+      </div>
       <MapSVG ref={svgRef}/>
 
     </div>
