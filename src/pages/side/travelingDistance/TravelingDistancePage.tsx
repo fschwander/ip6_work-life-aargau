b45ab435d1b5f2bd5a6068293f477699648a31d8
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-08 22:28:27 +0100

    updated margin of the content

--- src/pages/side/travelingDistance/TravelingDistancePage.tsx
+++ src/pages/side/travelingDistance/TravelingDistancePage.tsx
@@ -73,6 +73,7 @@ export const TravelingDistancePage: React.FC = () => {
 
   return (
     <div className='TravelingDistancePage'>
+      <div className='ContentContainer'>
         <h2>Der Aargau is gut vernetzt</h2>
         <p><b>DER KANTON, der im Zentrum der Schweiz und Europas liegt.</b></p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt ipsum ac erat mattis, in imperdiet
@@ -89,6 +90,7 @@ export const TravelingDistancePage: React.FC = () => {
 
       </div>
       <MapSVG ref={svgRef}/>
+      </div>
 
     </div>
   )
