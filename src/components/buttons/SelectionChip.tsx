0f1f0f125e0af396e2a8c9bf03bc33649197f331
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-10 10:14:11 +0100

    updated styling for the selection chips

--- src/components/buttons/SelectionChip.tsx
+++ src/components/buttons/SelectionChip.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 interface SelectionChipProps {
   text: string,
@@ -6,18 +6,20 @@ interface SelectionChipProps {
 }
 
 export const SelectionChip: React.FC<SelectionChipProps> = props => {
-
+  const [isActive, setIsActive] = useState(true);
 
   const onClick = () => {
     if (props.onClick !== undefined) {
       props.onClick();
     }
+    setIsActive(!isActive)
   }
 
   return (
 
 
-    <div className={`SelectionChip button chip-button`} onClick={onClick}>
+    <div className={`SelectionChip button chip-button ${isActive ? 'is-active' : ''}`}
+         onClick={onClick}>
       <p>{props.text}</p>
     </div>
 
