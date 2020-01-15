484a106b082e142438e332af0529f482f08986a0
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-15 15:48:50 +0100

    added and changed background picture

--- src/pages/intro/IntroductionPage.tsx
+++ src/pages/intro/IntroductionPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ReactComponent as SVGImage} from '../../res/imgs/AT_PSI_VILLIGEN.svg';
 import {TravelingDistancePage} from '../side/travelingDistance/TravelingDistancePage';
-import backgroundImg from './../../res/imgs/AT_LAUFENBURG.jpg'
+import backgroundImg from './../../res/imgs/AT_BADEN.jpg'
 import './IntroductionPage.scss';
 
 export const IntroductionPage: React.FC = () => {
