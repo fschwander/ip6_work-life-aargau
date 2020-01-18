ef7d9323df27b17338de4fc209816d58fb8cc233
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-01-18 15:12:52 +0100

    changed sequences of videos, updated content

--- src/pages/video/data/videoSlides.tsx
+++ src/pages/video/data/videoSlides.tsx
@@ -3,7 +3,6 @@ import videoAarau1 from "../../../res/videos/aarau1.mp4";
 import videoAarau2 from "../../../res/videos/aarau2.mp4";
 import {hikingTrails} from "./hikingTrails";
 import {mammut} from "./mammut";
-import {ReactComponent as svgAarau1} from "../../../res/videos/aarau1.svg";
 import {ReactComponent as svgRuine} from "../../../res/videos/baden1-ruine.svg";
 import {ReactComponent as svgRoof} from "../../../res/videos/baden1-roof.svg";
 import {ReactComponent as svgTower} from "../../../res/videos/baden1-tower.svg";
@@ -11,40 +10,8 @@ import {VideoSlideProps} from "../VideoSlide";
 
 export const videoSlides: Array<VideoSlideProps> = [
   {
-    className: 'aarau-slide1',
-    title: 'Die Stadt Aarau',
-    videoSrc: videoAarau1,
-    isMainPoint: true,
-    hoverPoints: [{
-      title: 'Kultur & Geschichte',
-      subtitle: 'Historische Highlights',
-      className: 'happy-new-year',
-      overlayData: hikingTrails,
-      svgComponent: svgAarau1,
-      lineLength: 60,
-      lineRotation: 70,
-      orientation: 'right'
-    },]
-  },
-  {
-    className: 'aarau-slide2',
-    title: 'Die Stadt Aarau',
-    videoSrc: videoAarau2,
-    hoverPoints: [{
-      title: 'Kultur & Geschichte',
-      subtitle: 'Historische Highlights',
-      className: 'culture-and-history',
-      overlayData: hikingTrails,
-      svgComponent: svgTower,
-      lineLength: 60,
-      lineRotation: 210,
-      orientation: 'right'
-    },]
-  },
-  {
     className: 'baden-slide1',
     title: 'Die Stadt Baden',
-    isMainPoint: true,
     videoSrc: videoBaden,
     hoverPoints: [
       {
@@ -78,5 +45,35 @@ export const videoSlides: Array<VideoSlideProps> = [
         orientation: 'right',
       }
     ]
-  }
+  },
+  {
+    className: 'aarau-slide1',
+    title: 'Die Stadt Aarau',
+    videoSrc: videoAarau1,
+    hoverPoints: [{
+      title: 'Kultur & Geschichte',
+      subtitle: 'Historische Highlights',
+      className: 'culture-and-history',
+      overlayData: hikingTrails,
+      svgComponent: svgRoof,
+      lineLength: 60,
+      lineRotation: 70,
+      orientation: 'right'
+    },]
+  },
+  {
+    className: 'aarau-slide2',
+    title: 'Die Stadt Aarau',
+    videoSrc: videoAarau2,
+    hoverPoints: [{
+      title: 'Kultur & Geschichte',
+      subtitle: 'Historische Highlights',
+      className: 'culture-and-history',
+      overlayData: hikingTrails,
+      svgComponent: svgTower,
+      lineLength: 60,
+      lineRotation: 210,
+      orientation: 'right'
+    },]
+  },
 ]
