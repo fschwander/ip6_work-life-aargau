8d8a277bc41cee36becc5656d38bf2743a711260
FabianSchwander <fabian.schwander@students.fhnw.ch>
2020-01-24 13:37:20 +0100

    setting video full size and resizing dynamically

--- src/components/containers/BackgroundVideo.tsx
+++ src/components/containers/BackgroundVideo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactPlayer from 'react-player';
 import {Spinner} from 'reactstrap';
 
@@ -10,7 +10,40 @@ interface FullScreenVideoProps {
 
 export const BackgroundVideo: React.FC<FullScreenVideoProps> = props => {
   const [isBuffering, setIsBuffering] = React.useState(false);
-  const videoRatio = 1920 / 1080;
+  const [size, setSize] = useState({videoWidth: 0, videoHeight: 0, translateX: 0});
+
+  const updateSize = () => {
+    const {innerWidth, innerHeight} = window;
+    const videoRatio = 1920 / 1080;
+    let width = innerHeight * videoRatio;
+
+    console.log(innerWidth, innerHeight);
+
+    if (innerWidth / innerHeight < videoRatio) {
+      console.log('if', videoRatio, innerWidth / innerHeight);
+      setSize({
+        videoWidth: innerHeight * videoRatio,
+        videoHeight: innerHeight,
+        translateX: innerWidth < width ? (width - innerWidth) / 2 : 0
+      });
+
+    } else {
+      console.log('else', videoRatio, innerWidth / innerHeight);
+      setSize({
+        videoWidth: innerWidth,
+        videoHeight: innerWidth / videoRatio,
+        translateX: innerWidth < width ? (width - innerWidth) / 2 : 0
+      });
+    }
+
+
+  }
+
+  useEffect(() => {
+    updateSize()
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
 
   return (
     <div className='BackgroundVideo'>
@@ -19,8 +52,8 @@ export const BackgroundVideo: React.FC<FullScreenVideoProps> = props => {
                    playing={props.playVideo}
                    muted={true}
                    playsinline={true}
-                   width={window.innerHeight * videoRatio}
-                   height={window.innerHeight}
+                   width={size.videoWidth}
+                   height={size.videoHeight}
                    onReady={() => setIsBuffering(false)}
                    onEnded={() => props.onVideoEnded()}/>
 
