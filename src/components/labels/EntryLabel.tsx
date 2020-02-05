8622af4a855b915e1624558b93c5971b7eabe504
FabianSchwander <fabian.schwander@students.fhnw.ch>
2020-02-05 11:24:02 +0100

    extracted code of line rotation to own service

--- src/components/labels/EntryLabel.tsx
+++ src/components/labels/EntryLabel.tsx
@@ -1,4 +1,5 @@
 import React, {CSSProperties} from 'react';
+import {lineRotationService} from "../../services/lineRotationService";
 
 interface EntryLabelProps {
   text: string,
@@ -10,78 +11,18 @@ interface EntryLabelProps {
 }
 
 export const EntryLabel: React.FC<EntryLabelProps> = props => {
-  let lineHeight = 1;
-  let deg = props.lineRotationInDeg;
 
-  if (deg < 0 || deg > 360) {
-    console.warn('Wrong values for line rotation: value should be between 0 an 360 but was ', props.lineRotationInDeg);
-  }
+  const deg = props.lineRotationInDeg;
+  const orientation = props.orientation;
+  const lineHeight = 1;
 
-  const getLineTransform = (): number => {
-    switch (props.orientation) {
-      case 'left':
-        if (deg < 90) {
-          return -lineHeight;
-        } else if (deg >= 90 && deg < 180) {
-          return 0
-        } else if (deg >= 180 && deg < 270) {
-          return -2 * lineHeight
-        } else {
-          return -lineHeight
-        }
-      case 'right':
-        if (deg < 90) {
-          return 0
-        } else if (deg >= 90 && deg < 180) {
-          return -lineHeight;
-        } else if (deg >= 180 && deg < 270) {
-          return -lineHeight;
-        } else {
-          return -2 * lineHeight;
-        }
-      default:
-        return 0;
-    }
-  }
-
-  const getTransformOrigin = (): string => {
-    switch (props.orientation) {
-      case 'left':
-        if (deg <= 90) {
-          return 'left top'
-        } else if (deg < 180) {
-          return 'left top'
-        } else if (deg < 270) {
-          return 'left bottom'
-        } else {
-          return 'left bottom'
-        }
-      case 'right':
-        if (deg <= 90) {
-          return 'right top'
-        } else if (deg < 180) {
-          return 'right top';
-        } else if (deg < 270) {
-          return 'right bottom'
-        } else {
-          return 'right bottom'
-        }
-      default:
-        return 'left bottom';
-    }
-  }
-
-  const getRealLineRotation = (): number => {
-    return (props.orientation === 'left' ? props.lineRotationInDeg : props.lineRotationInDeg + 180) % 360;
-  }
-
-  let lineTranslate = getLineTransform()
-  let lineTransformOrigin = getTransformOrigin()
-  let realLineRotation = getRealLineRotation()
+  const lineTranslate = lineRotationService.getLineTransform(orientation, deg, lineHeight);
+  const lineTransformOrigin = lineRotationService.getTransformOrigin(orientation, deg);
+  const realLineRotation = lineRotationService.getRealLineRotation(orientation, deg);
 
   const lineStyle = {
     width: `${props.lineWidth}px`,
-    transform: `translateY(${lineTranslate}px) rotate(${realLineRotation * -1}deg)`,
+    transform: `translateY(${lineTranslate}px) rotate(${realLineRotation}deg)`,
     transformOrigin: lineTransformOrigin
   }
 
---
+++ src/services/lineRotationService.tsx
@@ -0,0 +1,66 @@
+export class lineRotationService {
+
+  static getLineTransform = (orientation:string, deg: number, lineHeight: number): number => {
+
+    if (deg < 0 || deg > 360) {
+      console.warn('Line rotation should be between 0 an 360 but was', deg, 'degrees');
+    }
+
+    switch (orientation) {
+      case 'left':
+        if (deg < 90) {
+          return -lineHeight;
+        } else if (deg >= 90 && deg < 180) {
+          return 0
+        } else if (deg >= 180 && deg < 270) {
+          return -2 * lineHeight
+        } else {
+          return -lineHeight
+        }
+      case 'right':
+        if (deg < 90) {
+          return 0
+        } else if (deg >= 90 && deg < 180) {
+          return -lineHeight;
+        } else if (deg >= 180 && deg < 270) {
+          return -lineHeight;
+        } else {
+          return -2 * lineHeight;
+        }
+      default:
+        return 0;
+    }
+  }
+
+  static getTransformOrigin = (orientation:string, deg: number): string => {
+    switch (orientation) {
+      case 'left':
+        if (deg <= 90) {
+          return 'left top'
+        } else if (deg < 180) {
+          return 'left top'
+        } else if (deg < 270) {
+          return 'left bottom'
+        } else {
+          return 'left bottom'
+        }
+      case 'right':
+        if (deg <= 90) {
+          return 'right top'
+        } else if (deg < 180) {
+          return 'right top';
+        } else if (deg < 270) {
+          return 'right bottom'
+        } else {
+          return 'right bottom'
+        }
+      default:
+        return 'left bottom';
+    }
+  }
+
+  static getRealLineRotation = (orientation:string, deg: number): number => {
+    return (orientation === 'left' ? deg : deg + 180) % 360 * -1;
+  }
+
+}
