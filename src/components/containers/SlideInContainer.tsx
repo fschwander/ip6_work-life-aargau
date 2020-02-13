c885903b516ee5ddb3ca1e5b8a7f20b53e38cdd5
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-02-13 11:30:53 +0100

    Showing ScrollIndicator only if window is large enough to be scrolled

--- src/components/containers/SlideInContainer.tsx
+++ src/components/containers/SlideInContainer.tsx
@@ -1,4 +1,4 @@
-import React, {createRef, RefObject, useEffect, useState} from 'react';
+import React, {createRef, RefObject, useEffect, useRef, useState} from 'react';
 import closeIcon from '../../res/icons/arrow_right.svg';
 import {RoundButton} from '../buttons/RoundButton';
 import {ScrollIndicator} from '../widgets/ScrollIndicator';
@@ -9,36 +9,50 @@ interface SlideInContainerProps {
 }
 
 export const SlideInContainer: React.FC<SlideInContainerProps> = props => {
+  const isInitialMount = useRef(true);
+
   const scrollContainer: RefObject<HTMLDivElement> = createRef();
   const [showScrollIndicator, setShowScrollIndicator] = useState(true)
 
   const handleScroll = () => {
     if (scrollContainer.current) {
-      const windowHeight = window.innerHeight;
-      const scrollHeight = scrollContainer.current.scrollHeight;
       const scrollTop = scrollContainer.current.scrollTop;
-      const canBeScrolled = scrollHeight - windowHeight > 100;
       const wasScrolled = scrollTop > 100;
 
-      if ((showScrollIndicator && wasScrolled) || !canBeScrolled) {
+      if ((showScrollIndicator && wasScrolled)) {
         setTimeout(() => setShowScrollIndicator(false), 3000);
       }
     }
-  }
+  };
+
+  const initScroll = () => {
+    if (scrollContainer.current) {
+      const windowHeight = window.innerHeight;
+      const scrollHeight = scrollContainer.current.scrollHeight;
+      const canBeScrolled = scrollHeight - windowHeight > 100;
+
+      if (!canBeScrolled) {
+        setShowScrollIndicator(false)
+      }
+    }
+  };
 
   useEffect(() => {
-    handleScroll()
-  }, [])
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      initScroll()
+    }
+  });
 
   return (
     <div className={`SlideInContainer ${props.slideInDirection}`}>
-      {props.onCloseButtonClicked ?
-        <RoundButton onClick={props.onCloseButtonClicked} icon={closeIcon}/> : null}
-
       <div className='scroll-container' onScroll={handleScroll} ref={scrollContainer}>
         {props.children}
         <ScrollIndicator show={showScrollIndicator}/>
       </div>
+
+      {props.onCloseButtonClicked ?
+        <RoundButton onClick={props.onCloseButtonClicked} icon={closeIcon}/> : null}
     </div>
   )
 }
