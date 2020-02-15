00d2100a9ab18e42efe5bd0ed68fac48801b2a79
Matej Zmitko <matej.zmitko@students.fhnw.ch>
2020-02-15 17:29:27 +0100

    added content for education overlay in Aarau

--- src/pages/video/overlays/info-overlay-data/education.tsx
+++ src/pages/video/overlays/info-overlay-data/education.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import iconCommunity from '../../../../res/icons/community.svg';
+import iconResearch from '../../../../res/icons/research.svg';
 import {Constants} from '../../../../services/Constants';
+import iconEducation from '../../../../res/icons/education.svg'
+import imgEducation from '../../../../res/imgs/ol_education.jpg'
+import imgEducation2 from '../../../../res/imgs/ol_education2.jpg'
+import imgEducation3 from '../../../../res/imgs/ol_education3.jpg'
 import {PlaceholderChart, PopupOverlayInterface} from "../PopupOverlayInterface";
 
 export const education: PopupOverlayInterface = {
   type: Constants.TYPE_LIFE,
   header: {
     title: "Bildung & Forschung",
-    text: "",
+    text: "Der Aargau ist ein Bildungs- und Forschungskanton. Schon vor der Kantonsgründung wurde das erste weltliche Gymnasium der Schweiz in Aarau gegründet. Heute investiert der Kanton Aargau rund einen Drittel seiner Ausgaben in die Entwicklung und Weiterentwicklung seiner innovativen Bildungs- und Forschungslandschaft.",
     items: [
       {
-        text: "",
+        text: "Shulen & Berufsbildung",
         value: ""
       },
       {
-        text: "",
+        text: "Ingenieur- & Naturwissenschaften",
+        value: ""
+      },
+      {
+        text: "Industriewerkplatz",
         value: ""
       }
     ]
@@ -21,12 +31,16 @@ export const education: PopupOverlayInterface = {
 
   topics: [
     {
-      icon: "",
-      text: ""
+      icon: iconEducation,
+      text: "Bildung"
     },
     {
-      icon: "",
-      text: ""
+      icon: iconResearch,
+      text: "Forschung"
+    },
+    {
+      icon: iconCommunity,
+      text: "Gemeinschaft"
     }
   ],
 
@@ -37,18 +51,50 @@ export const education: PopupOverlayInterface = {
 
   details: [
     {
-      image: "",
-      title: "",
+      image: imgEducation,
+      title: "Hightech Aargau",
       items: [
         {
-          text: "",
-          value: ""
-        },
-        {
-          text: "",
-          value: ""
+          text: "Kernstück",
+          value: "Massnahmen für den Wissens- & Technologietransfers, die den Aargauer Unternehmen einen optimalen Zugang zu den besten verfügbaren Technologien ermöglichen."
         }
       ]
     },
+    {
+      image: imgEducation2,
+      title: "Campus Brugg-Windisch",
+      items: [
+        {
+          text: "Studiengebiete",
+          value: "Technik, Pädagogik und Wirtschaft"
+        },
+        {
+          text: "Anzahl Studierende",
+          value: "3'200"
+        },
+        {
+          text: "Anzahl Mitarbeitende",
+          value: "1'000"
+        }
+      ]
+    },
+    {
+      image: imgEducation3,
+      title: "Forschungsstandort",
+      items: [
+        {
+          text: "Forschungsgebiete",
+          value: "Materie und Material, Mensch und Gesundheit sowie Energie und Umwelt"
+        },
+        {
+          text: "Anzahl Mitarbeitende",
+          value: "1'500"
+        },
+        {
+          text: "Ort",
+          value: "zwischen Würenlingen und Villigen"
+        }
+      ]
+    }
   ]
 }
