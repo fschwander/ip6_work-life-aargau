3862ea3b0a3da78284b0241d9209a275d29e926c
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-02-21 13:23:40 +0100

    adjusting labels in svg map of Aarau and Brugg according to active filters

--- src/pages/menu/MenuFilterPage.tsx
+++ src/pages/menu/MenuFilterPage.tsx
@@ -1,3 +1,4 @@
+import * as d3 from 'd3';
 import React from 'react';
 import {SelectionChip} from '../../components/buttons/SelectionChip';
 import {Constants} from '../../services/Constants';
@@ -43,7 +44,7 @@ export const updateExclusiveFilter = (filters: Array<OptionFilter>, i: number, s
   setFilters(newFilters)
 }
 
-export const getDismissedFilters = (arrays: Array<Array<OptionFilter>>): {dismissedFilters: Array<{ type: string }>} => {
+export const getDismissedFilters = (arrays: Array<Array<OptionFilter>>): { dismissedFilters: Array<{ type: string }> } => {
   const dismissedFilters: Array<{ type: string }> = [];
   arrays.forEach(array => {
     array.forEach(filterItem => {
@@ -54,7 +55,19 @@ export const getDismissedFilters = (arrays: Array<Array<OptionFilter>>): {dismis
       }
     })
   })
-  return {'dismissedFilters' : dismissedFilters};
+  return {'dismissedFilters': dismissedFilters};
+}
+
+export const hideDismissedLabels = (arrays: Array<Array<OptionFilter>>) => {
+  const filterList = getDismissedFilters(arrays);
+
+  const svg = d3.selectAll('svg')
+  svg.selectAll('g').attr('opacity', 1)
+
+  filterList.dismissedFilters.forEach(f => {
+    const label = svg.selectAll('#' + f.type)
+    label.attr('opacity', 0)
+  })
 }
 
 export const mapOptionFilters = (filters: Array<OptionFilter>, setFilters: Function) => {
