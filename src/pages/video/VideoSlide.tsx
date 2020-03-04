ba6791aabf84dcbfc8517921745df51ce4f9c762
FabianSchwander <fabian.schwander@students.fhnw.ch>
2020-03-04 15:45:23 +0100

    set animation duration to length until when all labels are displayed

--- src/pages/video/VideoSlide.tsx
+++ src/pages/video/VideoSlide.tsx
@@ -154,7 +154,7 @@ export const VideoSlide: React.FC<VideoSlideProps> = props => {
 
       {animationStarted ?
         <div className={`progress-indicator-container ${getFadeInOutClass()}`}>
-          <ProgressIndicator animDurationInSec={30}
+          <ProgressIndicator animDurationInSec={(filteredInfoLabelItems.length + filteredPoiLabelItems.length) * animationStaggerInSec}
                              animationPaused={slidePaused}
                              onElementClicked={props.switchToNextSlide}/>
         </div> : null}
