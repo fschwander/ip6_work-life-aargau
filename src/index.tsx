5a058e473cce5c5695e3de9fc28945ec5f95f812
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-20 17:24:38 +0100

    implemented creation of react-snapshot

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {AppRouter} from './AppRouter';
 import * as serviceWorker from './services/serviceWorker';
+// @ts-ignore
+import { render } from 'react-snapshot';
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+
+render(<AppRouter/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
