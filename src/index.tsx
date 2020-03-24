22a2d8468b027ea87f437774fe711da80414599b
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-03-24 10:07:20 +0100

    Revert "removed dependency and creation of react-snapshot"
    This reverts commit 3c8a1e93

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {AppRouter} from './AppRouter';
 import * as serviceWorker from './services/serviceWorker';
+// @ts-ignore
+import { render } from 'react-snapshot';
 
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+render(<AppRouter/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
